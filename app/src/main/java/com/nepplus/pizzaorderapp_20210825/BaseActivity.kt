81d09a910063fb96e66b38ab6b5aab6433f3c6d9
김현지 <guswlhjk@naver.com>
2021-08-25 09:50:12 +0900

    베이스액티비티

---
+++ app/src/main/java/com/nepplus/pizzaorderapp_20210825/BaseActivity.kt
@@ -0,0 +1,14 @@
+package com.nepplus.pizzaorderapp_20210825
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
+
+}
