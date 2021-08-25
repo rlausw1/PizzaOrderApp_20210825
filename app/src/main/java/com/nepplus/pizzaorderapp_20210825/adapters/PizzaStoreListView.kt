886a7a413283609e8a10f1bd6fbab17c9c2b3ea2
김현지 <guswlhjk@naver.com>
2021-08-25 11:10:09 +0900

    이름 번ㄴ호 로고

--- app/src/main/java/com/nepplus/pizzaorderapp_20210825/adapters/PizzaStoreListView.kt
+++
@@ -1,7 +0,0 @@
-package com.nepplus.pizzaorderapp_20210825.adapters
-
-class PizzaStoreListView {
-
-
-
-}
---
+++ app/src/main/java/com/nepplus/pizzaorderapp_20210825/datas/StoreData.kt
@@ -0,0 +1,6 @@
+package com.nepplus.pizzaorderapp_20210825.datas
+
+class StoreData(val name : String,
+                val phoneNum : String,
+                val logoURL : String ) {
+}
